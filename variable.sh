4dfdacd4190cb9fd039f1721a67a0534e9e7b1fc
mj-gsl <gsl.produktiion@gmail.com>
2023-11-07 14:58:42 +0100

    full changed

--- variable.sh
+++ variable.sh
@@ -25,4 +25,21 @@ fi
 #---------- Erstellen eines Arrays -------------       
 fruit_basket=("Apfel" "Banane" "Orange" "Erdbeere" "Kiwi" "Mango")
 echo "Das gesamt Array: ${fruit_basket[@]}"
-echo "Die zweite Frucht aus Array: ${fruit_basket[1]}"
+echo -e "\e[32mDie zweite Frucht aus Array: ${fruit_basket[1]}\e[0m"
+#---------- Umgebungsvariablen ---------
+echo -e "\e[31mThis is the path Address:\e[0m $PATH"
+#---------- Verändern von Variablen: ----------
+counter=15
+counter=$((counter + 1))
+echo -e "\e[33mThe updated value of counter variable is:\e[0m $counter"
+#------------ Schreibgeschützte Variable ----------------------
+constant_value="This is a constant variable"
+readonly constant_value
+constant_value="New Value"
+#------------ Ersetzen von Kommandos ----------------
+date_time=$(date)
+echo -e "\e[34mThe current Date and Time: $date_time\e[0m"
+#------------ Reihenfolge umkehren ------------
+read -p "input a string: " input_string
+reversed_string=$(awk '{ for(i=length; i!=0; i--) x=x substr($0,i,1);}END{print x;}' <<< "$input_string")
+echo "the reversed string is: $reversed_string"
